3ea6bac0e3eb98453ccf28c54edc3dc090891281
tpecherkina <tpecherkina18@gmail.com>
2021-07-11 21:49:53 -0400

    Init

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,13 +1,22 @@
+'use strict'
+
 function solveEquation(a, b, c) {
-  let arr;
-  // код для задачи №1 писать здесь
-  return arr; // array
+
+  let arr = [];
+  let x1 = 0;
+  let x2 = 0;
+ 
+  let d = (b**b) - (4 * a * c); 
+  
+   if (d === 0) {
+    x1 = -b / (2 * a);
+    arr.unshift(x1);
+  } else if (d > 0) {
+    x1 = (-b + Math.sqrt(d) ) / (2 * a);
+    x2 = (-b - Math.sqrt(d) ) / (2 * a);
+     arr.unshift(x1, x2);
+  }
+  return arr;
 }
 
-function calculateTotalMortgage(percent, contribution, amount, date) {
-  let totalAmount;
 
-  // код для задачи №2 писать здесь
-
-  return totalAmount;
-}
