b5cfd38cc6fd066d7e1eed60979fc4ea84dfa361
tpecherkina <tpecherkina18@gmail.com>
2021-08-09 23:38:47 -0400

    Arrays

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,20 @@
 function compareArrays(arr1, arr2) {
   let result;
-
-  // Ваш код
-
-  return result; // boolean
+  
+ if (arr1.length === arr2.length) && arr1.every(function(element, index)) {
+      return element === arr2[index]{ 
+  result = true;
+} else {
+  result = false; 
+}
+ }
+return result; // boolean
 }
 
 function advancedFilter(arr) {
   let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  
+  resultArr = arr.filter(index => index >= 0 && index % 3===0).map(index => index * 10);
+  return resultArr;
 }
+  
