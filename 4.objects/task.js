a6f4b4600695451bcfc28d1f005cd416ce951307
tpecherkina <tpecherkina18@gmail.com>
2021-08-11 22:07:39 -0400

    Objects

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,46 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.age = age,
+  this.gender = gender,
+  this.name = name
 
 }
+
+let student1 = new Student("Tanya", 'female', 32);
+let student2 = new Student("Masha", "female", 24);
+let student3 = new Student("Piotr", "male", 38);
 
 Student.prototype.setSubject = function (subjectName) {
-  //ваш код
+   this.subject = subjectName;
+  }
+  
+ Student.prototype.addMark = function (mark) {
+    if(this.marks === undefined){ 
+   this.marks = [mark]
+    } else {
+       this.marks.push(mark)
+    }
+ }
+ 
+Student.prototype.addMarks = function (...rest) {
+  this.marks = [...rest];
 }
 
-// ваш код для остальных методов
+
+Student.prototype.getAverage = function () {
+  let sum = 0;
+  let count = 0;
+  for (let i = 0; i < this.marks.length; i++) {
+ sum += this.marks[i];
+}
+  let average = sum / this.marks.length;
+  
+  return average;
+}
+
+Student.prototype.exclude = function (reason) {
+  delete this.subject;
+  delete this.marks;
+    this.excluded = reason;
+}
+
+
